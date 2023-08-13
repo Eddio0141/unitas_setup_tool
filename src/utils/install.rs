ca6e2f80e7e623beb0fb92162669617cd7e3ecc2
8Kat <48191140+8kat@users.noreply.github.com>
2023-08-13 17:41:04 -0500

    keep branches consistent

--- src/utils/install.rs
+++ src/utils/install.rs
@@ -286,11 +286,6 @@ async fn install_unitas(game_dir: &Path, unitas_version: DownloadVersion) -> Res
             dest_dir.display()
         );
 
-        if !source_dir.exists() {
-            warn!("{} does not exist, skipping", source_dir.display());
-            continue;
-        }
-
         utils::fs::copy_dir_all(&source_dir, &dest_dir, true).with_context(|| {
             format!(
                 "Could not copy UniTAS folder from {} to {}",
