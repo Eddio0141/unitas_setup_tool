e9fdb06f1953fa4870f8111e1fa314f495e6589a
Eddio0141 <eddio0141@gmail.com>
2023-08-15 14:48:46 +0100

    added flake for building

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Development environment for this project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { self, flake-parts, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { pkgs, ... }: {
+        devShells.default =
+          pkgs.mkShell {
+            packages = with pkgs; [
+              rustc
+              cargo
+            ];
+          };
+      };
+    };
+}
