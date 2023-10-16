1fb37ed58e7f3ac3e2be23928e5cda47c96931bf
Eddio0141 <eddio0141@gmail.com>
2023-10-16 02:02:33 +0100

    changed test to pre-push instead

--- flake.nix
+++ flake.nix
@@ -43,6 +43,7 @@
                       entry = "${pkgs.cargo}/bin/cargo test";
                       fail_fast = true;
                       pass_filenames = false;
+                      stages = [ "pre-push" ];
                     };
                   };
                 }
