c6da22e8d9390249d7d9464a7b55875fecd73604
Eddio0141 <eddio0141@gmail.com>
2024-01-01 20:57:24 +0000

    fix clippy error

--- src/error.rs
+++ src/error.rs
@@ -7,15 +7,15 @@ pub enum Error {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
     #[error(transparent)]
-    PathError(#[from] paths::error::Error),
+    Path(#[from] paths::error::Error),
     #[error(transparent)]
-    HistoryError(#[from] history::error::Error),
+    History(#[from] history::error::Error),
     #[error(transparent)]
-    GameDirError(#[from] game_dir::error::Error),
+    GameDir(#[from] game_dir::error::Error),
     #[error("Failed to download UniTAS: {0}")]
-    DownloadUniTASError(#[from] download::error::Error),
+    DownloadUniTAS(#[from] download::error::Error),
     #[error(transparent)]
-    LocalVersionsError(#[from] local_versions::Error),
+    LocalVersions(#[from] local_versions::Error),
     #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
